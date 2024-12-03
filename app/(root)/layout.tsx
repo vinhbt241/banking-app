bd3b25069bf176efc8a17063319cd0e180be3b88
vinhbt241 <vinhbuith32412000@gmail.com>
2024-12-03 22:53:19 +0700

    add navbar and sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,30 @@
+import MobileNav from "@/components/MobileNav"
+import Sidebar from "@/components/Sidebar"
+import Image from "next/image"
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const loggIn = { firstName: "Vinh", lastName: "Bui" }
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src={"/icons/logo.svg"}
+            width={30}
+            height={30}
+            alt="Menu icon"
+          />
+          <MobileNav user={loggIn} />
+        </div>
+        {children}
+      </div>
     </main>
   )
 }
